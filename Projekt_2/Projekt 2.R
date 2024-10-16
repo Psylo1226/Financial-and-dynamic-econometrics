6740da20d12fbb3d33969975b4f0d9b70aeaf334
Piotr Stawarski <staw2000@gmail.com>
2024-10-16 16:21:25 +0200

    Groundwork

---
+++ Projekt_2/Projekt 2.R
@@ -0,0 +1,40 @@
+library("tidyverse")
+library("moments")
+library("tseries")
+library("forecast")
+izo <- read.csv("izo_d.csv")
+
+#Stopa logarytmiczna
+st_l <- function(a,b) {
+  return(log(a/b))
+}
+
+indeksy <- izo[,c(1,5)]
+indeksy <- indeksy %>% map_df(rev)
+prognoza <- head(indeksy,4)
+dane <- tail(indeksy,-4)
+
+szereg <- c()
+for (i in 1:(nrow(dane)-1)) {
+  szereg <- c(szereg_l,as.numeric(st_l(dane[i,2],dane[i+1,2])))
+}
+
+#Test ADF
+adf.test(szereg)
+#Bardzo niskie p-value -> szereg stacjonarny
+
+#ARMA
+model <- auto.arima(szereg)
+summary(model)
+#Zaproponowany model: ARMA(2,2)
+
+#Reszty modelu
+res <- residuals(model)
+Box.test(res, type = "Ljung-Box")
+#P-value większe od 0.05 -> brak aurokorelacji
+jarque.bera.test(res)
+#P-value mniejsze od 0.05 -> reszty nie mają rozkładu normalnego
+qqnorm(res)
+
+
+
