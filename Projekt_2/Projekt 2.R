73fa8c66e037dfb441ad5bf649c38274f8f995ed
Piotr Stawarski <staw2000@gmail.com>
2024-11-03 15:38:54 +0100

    update

--- Projekt_2/Projekt 2.R
+++ Projekt_2/Projekt 2.R
@@ -9,18 +9,18 @@ st_l <- function(a,b) {
   return(log(a/b))
 }
 
-indeksy <- izo[,c(1,5)]
-indeksy <- indeksy %>% map_df(rev)
-odłożone_stopy <- head(indeksy,5)
-odłożone_ceny <- log(head(indeksy,4)[2])
-dane <- tail(indeksy,-4)
+dane <- izo[,c(1,5)]
+indeksy <- dane %>% map_df(rev)
 
-stopy <- c()
-for (i in 1:(nrow(dane)-1)) {
-  stopy <- c(stopy,as.numeric(st_l(dane[i,2],dane[i+1,2])))
+dt <- c()
+for (i in 1:(nrow(indeksy)-1)) {
+  dt <- c(dt,as.numeric(st_l(indeksy[i,2],indeksy[i+1,2])))
 }
+stopy <- rev(tail(dt, -4))
+ceny <- head(log(dane[2]), -4)
 
-ceny <- log(dane[2])
+stopy_oct <- rev(head(dt,4))
+ceny_oct <- tail(log(dane[2]), 4)
 
 #Test ADF
 adf.test(stopy)
@@ -29,7 +29,7 @@ adf.test(stopy)
 #ARMA
 model <- auto.arima(stopy)
 summary(model)
-#Zaproponowany model: ARMA(2,3)
+#Zaproponowany model: ARMA(3,2)
 
 #Reszty modelu
 res <- residuals(model)
@@ -39,15 +39,28 @@ jarque.bera.test(res)
 #P-value mniejsze od 0.05 -> reszty nie mają rozkładu normalnego
 qqnorm(res)
 
-#Prognoza logarytmicznych stóp dla 4 przyszłych notowań
-prognoza_zwrotow <- forecast(model,4)$mean
+#Prognoza logarytmicznych stóp dla 4 przyszłych notowań i
+#porównanie z odłożonymi wartościami
+stopy_wyniki <- data.frame(
+  Odłożone_Wartości = unname(stopy_oct),
+  Początek_Przedziału = forecast(model,4)$lower[, 2],
+  Koniec_Przedziału = forecast(model,4)$upper[, 2],
+  Średnia_Prognoza = forecast(model,4)$mean
+)
 
 #Model dla logarytmów cen
 model2 <- auto.arima(ceny)
 summary(model2)
 
-prognoza_cen <- forecast(model2,4)$mean
+#Porównanie z odłożonymi wartościami
+ceny_wyniki <- data.frame(
+  Odłożone_Wartości = unname(ceny_oct),
+  Początek_Przedziału = forecast(model2,4)$lower[, 2],
+  Koniec_Przedziału = forecast(model2,4)$upper[, 2],
+  Średnia_Prognoza = forecast(model2,4)$mean
+)
 
+exp(prognoza_cen)
 
 
 
