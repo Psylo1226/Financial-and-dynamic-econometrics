f40f948000fec58f1647ec105095a661168d535a
Piotr Stawarski <staw2000@gmail.com>
2025-01-29 20:19:18 +0100

    updates

--- Projekt_7/projekt7.R
+++ Projekt_7/projekt7.R
@@ -1,6 +1,7 @@
 library(dplyr)
 library(reshape2)
 library(ggplot2)
+library(tidyr)
 
 wig <- read.csv("wig20_d.csv")
 dax <- read.csv("dax_d.csv")
@@ -18,6 +19,16 @@ merged <- wig |>
   inner_join(moex, by = "Data")
 
 colnames(merged) <- c("Data","WIG","DAX","CAC","MOEX")
+merged$Data <- as.Date(merged$Data)
+
+pivot_merged <- pivot_longer(merged, cols = -Data, names_to = "Indeks", values_to = "val")
+
+ggplot(pivot_merged, aes(x = Data, y = val, color = Indeks)) +
+  geom_line() +
+  geom_vline(xintercept = as.numeric(as.Date("2020-03-01")), linetype = "dashed", color = "black", linewidth = 0.6) +
+  geom_vline(xintercept = as.numeric(as.Date("2022-02-24")), linetype = "dashed", color = "black", linewidth = 0.6) +
+  labs(title = "Wartości zamknięcia", x = "Data", y = "Wartość") +
+  theme_minimal()
 
 zwroty <- merged %>%
   dplyr::mutate(
@@ -27,13 +38,21 @@ zwroty <- merged %>%
     )
   )
 
-zwroty <- zwroty[-1,]
-zwroty$Data <- as.Date(zwroty$Data)
+zwroty <- na.omit(zwroty)
+
+pivot_zwroty <- pivot_longer(zwroty, cols = -Data, names_to = "Indeks", values_to = "val")
+
+ggplot(pivot_zwroty, aes(x = Data, y = val, color = Indeks)) +
+  geom_line() +
+  geom_vline(xintercept = as.numeric(as.Date("2020-03-01")), linetype = "dashed", color = "black", linewidth = 0.6) +
+  geom_vline(xintercept = as.numeric(as.Date("2022-02-24")), linetype = "dashed", color = "black", linewidth = 0.6) +
+  labs(title = "Wartości zamknięcia", x = "Data", y = "Wartość") +
+  theme_minimal()
 
 #Covid
 
-z1 <- zwroty[zwroty$Data < as.Date("2020-01-01"),-1]
-z2 <- zwroty[zwroty$Data <= as.Date("2020-06-30") & zwroty$Data >= as.Date("2020-01-01"),-1]
+z1 <- zwroty[zwroty$Data < as.Date("2020-03-01"),-1]
+z2 <- zwroty[zwroty$Data <= as.Date("2020-06-30") & zwroty$Data >= as.Date("2020-03-01"),-1]
 z3 <- zwroty[zwroty$Data <= as.Date("2021-12-31") & zwroty$Data >= as.Date("2020-07-01"),-1]
 
 cor1 <- cor(z1)
@@ -43,8 +62,8 @@ cor3 <- cor(z3)
 #Wojna na Ukrainie
 
 y1 <- zwroty[zwroty$Data < as.Date("2022-02-24") & zwroty$Data >= as.Date("2021-01-01"),-1]
-y2 <- zwroty[zwroty$Data <= as.Date("2022-05-31") & zwroty$Data >= as.Date("2022-02-24"),-1]
-y3 <- zwroty[zwroty$Data <= as.Date("2024-12-31") & zwroty$Data >= as.Date("2022-06-01"),-1]
+y2 <- zwroty[zwroty$Data <= as.Date("2022-03-24") & zwroty$Data >= as.Date("2022-02-24"),-1]
+y3 <- zwroty[zwroty$Data <= as.Date("2024-12-31") & zwroty$Data >= as.Date("2022-03-25"),-1]
 
 cor4 <- cor(y1)
 cor5 <- cor(y2)
